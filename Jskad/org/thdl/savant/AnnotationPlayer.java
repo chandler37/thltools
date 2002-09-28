2c594a8f7e91328e46dd74e5333a981e6714ae52
dchandler <>
2002-09-28 00:51:54 +0000

    DOH!
    I should have imported these into Jskad/source, not Jskad/.

--- Jskad/org/thdl/savant/AnnotationPlayer.java
+++
@@ -1,7 +0,0 @@
-package org.thdl.savant;
-
-public interface AnnotationPlayer extends java.util.EventListener
-{
-	public void startAnnotation(String id);
-	public void stopAnnotation(String id);
-}
