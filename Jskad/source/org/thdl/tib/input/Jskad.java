308981cbbb777ffc0a17b11acb008b20d27eb9a3
dchandler <>
2003-06-30 02:22:09 +0000

    I've validated by hand the TM<->TMW mappings.  A few things changed, so no previous TM->TMW conversions can be trusted.

--- Jskad/source/org/thdl/tib/input/Jskad.java
+++ Jskad/source/org/thdl/tib/input/Jskad.java
@@ -490,6 +490,20 @@ public class Jskad extends JPanel implements DocumentListener {
             toolsMenu.add(DevelItem);
         }
 
+// I used this when I validated the TM<->TMW mappings; I made
+// dp.romanAttributeSet public to do so:
+//
+//          if (ThdlOptions.getBooleanOption("thdl.add.developer.options.to.menu")) {
+//              toolsMenu.addSeparator();
+//              JMenuItem DevelItem = new JMenuItem("Insert All TM Glyphs in Doc");
+//              DevelItem.addActionListener(new ThdlActionListener() {
+//                      public void theRealActionPerformed(ActionEvent e) {
+//                          ((TibetanDocument)dp.getDocument()).insertAllTMGlyphs2(dp.romanAttributeSet);
+//                      }
+//                  });
+//              toolsMenu.add(DevelItem);
+//          }
+
         if (ThdlOptions.getBooleanOption("thdl.add.developer.options.to.menu")) {
             toolsMenu.addSeparator();
             JMenuItem DevelItem = new JMenuItem("Debug dump to standard output");
